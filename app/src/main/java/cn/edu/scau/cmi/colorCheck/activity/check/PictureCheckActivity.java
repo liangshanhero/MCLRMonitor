e78b2b4bc7b25fb2b4c78bfda5e7596c29c567c4
xt_lzq@126.com <liang123456>
2018-11-19 13:17:54 +0800

    (1) understand asyncTask, and use it to update UI successfully. (2) modify code later;

--- app/src/main/java/cn/edu/scau/cmi/colorCheck/activity/check/PictureCheckActivity.java
+++ app/src/main/java/cn/edu/scau/cmi/colorCheck/activity/check/PictureCheckActivity.java
@@ -3,14 +3,22 @@ package cn.edu.scau.cmi.colorCheck.activity.check;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import cn.edu.scau.cmi.colorCheck.R;
 import cn.edu.scau.cmi.colorCheck.dao.asyncTask.ProjectAsyncTask;
@@ -30,6 +38,11 @@ public class PictureCheckActivity extends AppCompatActivity {
     Spinner ruleSpinner;
     private static  List<Project> projectList;
     private static List<Rule> ruleList;
+    private Handler handler;
+    ArrayAdapter<Project> projectAdapter;
+    ArrayAdapter<Rule> ruleAdapter;
+    private TextView tv_random;
+    private Looper looper;
 
 
 
@@ -38,42 +51,22 @@ public class PictureCheckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_check);
         initView();
-    }
 
-    private void initView(){
-//        （1）摄像头处理
-        cameraPictureSurfaceView =findViewById(R.id.picture_check_surface);
-        //                在这里添加图片的显示部分，在结果界面中显示这个图表。
-//                保持图片文件，如过不能保存图片，需要在手机中设置读取权限
-        cameraPictureSurfaceView.setTouchListener(new TouchListenerAdapter(){
-            @Override
-            public void showPicture(Bitmap bitmap) {
-//                保存图片到本地
-                try {
-                    FileUtil.saveMyBitmap(bitmap,"20181118");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Intent intent=new Intent(PictureCheckActivity.this, PictureCheckResultActivity.class);
-                startActivity(intent);
-            }
-        });
-
-//TODO        （2）所有检测项目的获取,将项目列表给projectSpinner选择。
-//TODO 好像也可以不用传context进去，最后通过静态方法获取值应该可以，待测试!!!!!!
-//        TODO 也应该可以使用一个自定义的service类，这个service再来调用AsyncTask任务，待测试？？？？？？这样应该可以简化代码
+//异步获取数据，并在异步任务中显示输出，设置界面
         ProjectAsyncTask projectAsyncTask=new ProjectAsyncTask(this);
         projectAsyncTask.execute();
-        projectList=ProjectAsyncTask.getAllProject();
-        ArrayAdapter<Project> projectAdapter=new ArrayAdapter<Project>(this, android.R.layout.simple_list_item_1,projectList);
-        projectSpinner.setAdapter(projectAdapter);
 
-//TODO        (3)应该是项目选择后，根据项目的情况出现规则的值，现在都选，后面在修改。
-        RuleAsyncTask ruleAsyncTask=new RuleAsyncTask(this);
-        ruleAsyncTask.execute();
-        ruleList=RuleAsyncTask.getAllRules();
-        ArrayAdapter<Rule> ruleAdapter=new ArrayAdapter<Rule>(this, android.R.layout.simple_list_item_1,ruleList);
-        ruleSpinner.setAdapter(ruleAdapter);
+
+    }
+
+
+
+
+    private void initView(){
+//        第一步：初始化摄像头
+        initCamera();
+//第二部：初始化project和rule的spinner。
+//        initSpinner();
 
 //TODO (4)点击项目后，ruleSpinner的内容应该改变
 
@@ -90,6 +83,41 @@ public class PictureCheckActivity extends AppCompatActivity {
 //        });
     }
 
+    private void initSpinner() {
+        //TODO 利用消息队列更新界面的内容
+        projectList=ProjectAsyncTask.getAllProject();
+        System.out.println("开始的时候，所有的项目是："+projectList);
+        projectAdapter=new ArrayAdapter<Project>(this, android.R.layout.simple_list_item_1,projectList);
+//        projectSpinner.setAdapter(projectAdapter);
+
+        ruleList=RuleAsyncTask.getAllRules();
+        ruleAdapter=new ArrayAdapter<Rule>(this, android.R.layout.simple_list_item_1,ruleList);
+//        ruleSpinner.setAdapter(ruleAdapter);
+    }
+
+
+
+    private void initCamera() {
+        //        （1）摄像头处理
+        cameraPictureSurfaceView =findViewById(R.id.picture_check_surface);
+        //                在这里添加图片的显示部分，在结果界面中显示这个图表。
+//                保持图片文件，如过不能保存图片，需要在手机中设置读取权限
+        cameraPictureSurfaceView.setTouchListener(new TouchListenerAdapter(){
+            @Override
+            public void showPicture(Bitmap bitmap) {
+//                保存图片到本地,文件名称是yyyyMMddhhmmss
+                String fileName=new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+                try {
+                    FileUtil.saveMyBitmap(bitmap,fileName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Intent intent=new Intent(PictureCheckActivity.this, PictureCheckResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     // TODO  待完成：点击界面上的检测按钮后打开结果页面
     private void picture_check(View view){
 //      在这里将图片的检测结果作为参数在结果界面中显示这个图表。
@@ -97,4 +125,37 @@ public class PictureCheckActivity extends AppCompatActivity {
         Intent intent=new Intent(PictureCheckActivity.this, PictureCheckResultActivity.class);
         startActivity(intent);
     }
+// 启用线程，调用异步任务获取数据，并利用hanlder更新spinner
+//    @Override
+//    public void run() {
+//
+//        //TODO        （2）所有检测项目的获取,将项目列表给projectSpinner选择。
+////TODO 好像也可以不用传context进去，最后通过静态方法获取值应该可以，待测试!!!!!!
+////        TODO 也应该可以使用一个自定义的service类，这个service再来调用AsyncTask任务，待测试？？？？？？这样应该可以简化代码
+//        ProjectAsyncTask projectAsyncTask=new ProjectAsyncTask(this);
+//        projectAsyncTask.execute();
+//
+//        //TODO        (3)应该是项目选择后，根据项目的情况出现规则的值，现在都选，后面在修改。
+//        RuleAsyncTask ruleAsyncTask=new RuleAsyncTask(this);
+//
+//        ruleAsyncTask.execute();
+//        while (!Thread.currentThread().isInterrupted()){
+//            Message m=handler.obtainMessage();
+//            m.what=1;
+//            handler.sendMessage(m);
+//            try{
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/cn/edu/scau/cmi/colorCheck/dao/asyncTask/ProjectAsyncTask.java
+++ app/src/main/java/cn/edu/scau/cmi/colorCheck/dao/asyncTask/ProjectAsyncTask.java
@@ -1,6 +1,10 @@
 package cn.edu.scau.cmi.colorCheck.dao.asyncTask;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.edu.scau.cmi.colorCheck.R;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -29,11 +33,19 @@ public class ProjectAsyncTask extends AsyncTask <String,Void,String>{
         try {
             Request request=HttpUtil.getGetRequest("Project");
             String responseString = HttpUtil.getJsonDataFromWeb(request);
+            System.out.println("ProjectAsyncTask的响应字符串是："+responseString);
             allProject = new Gson().fromJson(responseString,new TypeToken<List<Project>>(){}.getType());
             return "获取数据成功";
         } catch (Exception e) {
             return "错误";
         }
+    }
+
+//利用该方法，可以更新界面的内容，更简单的方法
+    protected void onPostExecute(String str){
+        System.out.println("异步任务完成后所获取的所有的项目是："+allProject.toString());
+        TextView textView=pictureCheckActivity.findViewById(R.id.picture_check_random);
+        textView.setText("异步任务完成后所获取的所有的项目是："+allProject.toString());
     }
 }
 
